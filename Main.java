b891ce17878d1dbc485de5b5aef8ed651da38438
s166harth <siddharthsagar2019@gmail.com>
2022-05-25 17:00:01 +0530

    parcals triangle 2 (Inefficient)

--- Main.java
+++ Main.java
@@ -1,18 +1,23 @@
 import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
-   public List<List<Integer>> generate(int numRows) {
-       List<List<Integer>> list = new ArrayList<>();
-       for (int i = 0; i < numRows; i++) {
-           list.add(new ArrayList<>());
-           for (int j = 0; j < i + 1; j++) {
-               if (j == 0 || j == i) {
-                   list.get(i).add(1);
-               }else {
-                   list.get(i).add(list.get(i-1).get(j)+list.get(i-1).get(j-1));
-               }
-
-           }
-       }return list;
+  public static int bi(int n,int k)
+   {
+      if(n==k||k==0) return 1;
+      else{
+         return bi(n-1,k)+bi(n-1,k-1);
+      }
    }
+   
+
+   public List<Integer> getRow(int rowIndex) {
+       List<Integer> ans = new ArrayList<Integer>();
+      for(int i=0;i<rowIndex;i++)
+      {
+         ans.add(bi(rowIndex,i));
+      }
+      return ans;
+   }
+   
 }
