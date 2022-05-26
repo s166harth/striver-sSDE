0114e1ce3eb97e906ef2872be6580f732cf95d40
s166harth <siddharthsagar2019@gmail.com>
2022-05-26 18:13:28 +0530

    dutch flag algorithm

--- Main.java
+++ Main.java
@@ -1,15 +1,27 @@
 class Solution {
-   public int maxProfit(int[] prices) {
-       int min = 10000000;
-       int today=0;
-       int ans=0;
-       for(int i=0;i<prices.length;i++)
-       {
-           min = Math.min(min,prices[i]);
-           today = prices[i]-min;
-           ans=Math.max(ans,today);
+
+   public void sortColors(int[] nums) {
+      int li=0;
+      int mid=0;
+      int hi=nums.length-1;
+       while(mid<=hi){
+         if(nums[mid]==0){
+             int temp=nums[li];
+             nums[li]=nums[mid];
+             nums[mid]=temp;
+             mid++;
+             li++;
+         }
+               else if(nums[mid]==1){
+                       mid++;
+               }
+        else{
+            int temp2=nums[mid];
+             nums[mid]=nums[hi];
+             nums[hi]=temp2;
+             hi--;       
+             } 
        }
-       return ans;
-       
    }
-}
+   
+   }
