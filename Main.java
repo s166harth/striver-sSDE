23672e42b7e0d34f05e5685689a5774ead567459
s166harth <siddharthsagar2019@gmail.com>
2022-05-28 21:11:20 +0530

    duplicates using linked list cycle method and not hashmaps

--- Main.java
+++ Main.java
@@ -1,28 +1,22 @@
-class Solution {
-   static void swap(int[] num,int m,int n)
-   {
-      int temp = num[n];
-      num[n] = num[m];
-      num[m] = temp;
-   }
-   public void merge(int[] nums1, int m, int[] nums2, int n) {
-         for(int i=0;i<n;i++)
-         {
-            nums1[m+i]=nums2[i];
 
-         }
-         int gap=(m+n)/2 + (m+n)%2;
-         int i=0;
-         
-        while (true) {
-            i = 0;
-            while (i < nums1.length-gap) {
-                if (nums1[i] > nums1[i+gap]) 
-                    swap(nums1, i, i+gap);
-                i++;
-            }
-            if (gap == 1) break;
-            gap = gap / 2 + gap % 2;
+class TUF {
+    public static int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        fast = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-   }
+        return slow;
+    }
+    public static void main(String args[]) {
+        int arr[] = {1,3,4,2,3};
+        System.out.println("The duplicate element is " + findDuplicate(arr));
+    }
 }
