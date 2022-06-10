40a06dab90767cc3275b81d1d7889f5f8b587d88
s166harth <siddharthsagar2019@gmail.com>
2022-06-10 16:52:35 +0530

    two sum using hashmap

--- Main.java
+++ Main.java
@@ -1,13 +1,25 @@
-class Solution {
-    public int reversePairs(int[] nums) {
-        int ans=0;
-        for(int i=0;i<nums.length-1;i++)
-        {
-            for(int j=i+1;j<nums.length;j++)
-            {
-                if(nums[i]>nums[j]*2)ans++;
-            }
+import java.util.HashMap;
 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[]  ans = new int[2];
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            hm.put(nums[i],i);
+        }
+        int[] arr = new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            arr[i]=target-nums[i];
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            if(hm.containsKey(arr[i]))
+            {
+                ans[0]=i;
+                ans[1]=hm.get(arr[i]);
+            }
         }
         return ans;
     }
