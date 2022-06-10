dbdb46cf1af2cf0346fd0244d531570f13d3ae5f
s166harth <siddharthsagar2019@gmail.com>
2022-06-10 15:36:31 +0530

    majority element 2(n/3)

--- Main.java
+++ Main.java
@@ -1,30 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
 class Solution {
-    public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++)
+    public List<Integer> majorityElement(int[] nums) {
+    List<Integer> ans = new ArrayList<>();
+    HashMap<Integer,Integer> hm=new HashMap<>();
+    for(int i=0;i<nums.length;i++)
+    {
+        if(hm.containsKey(nums[i]))
         {
-            if(!map.containsKey(nums[i]))
-            {
-                map.put(nums[i], 1);
-                
-            }
-            else
-            {
-                int value = map.get(nums[i]);
-                map.put(nums[i], value+1);
-            }
+            int f=hm.get(nums[i]);
+            hm.put(nums[i], f++);
         }
-        
-        int element = 0;
-        
-        for(Map.Entry<Integer, Integer> m : map.entrySet())
+        else
         {
-            if(m.getValue() > (nums.length/2))
-            {
-                element = m.getKey();
-            }
+            hm.put(nums[i], 1);
         }
-        
-        return element;
+    }
+    int floor = (int)Math.floor(nums.length/3);
+    Set<Integer> keys = hm.keySet();
+    for(Integer key: keys)
+    {
+        if(hm.get(key)>floor)
+        {
+            ans.add(key);
+        }
+    }
+    return ans;
     }
 }
