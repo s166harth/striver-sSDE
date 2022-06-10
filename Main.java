d7eb7ed872f140edc5fe32a33e3a278911f9d6da
s166harth <siddharthsagar2019@gmail.com>
2022-06-11 01:18:06 +0530

    longest sequence using hashmaps where we check if one element smaller is present or not and build up from minimum number of the sequence to avoid looping

--- Main.java
+++ Main.java
@@ -1,36 +1,27 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (nums.length < 4) return res;
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            if (i == 0 || nums[i - 1] != nums[i]) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (j == i + 1 || nums[j - 1] != nums[j]) {
-                        twoSum(nums, i, j, res, target);
-                    }
+    public int longestConsecutive(int[] nums) {
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i: nums)
+            set.add(i);
+        
+        int max = 0;
+        
+        for(int i : set){
+            if(!set.contains(i - 1)){
+                int j = i;
+                while(set.contains(j)){
+                    j++;
                 }
+                
+                max = Math.max(max, j - i);
             }
         }
-        return res;
-    }
-    
-    public void twoSum(int[] nums, int first, int second, List<List<Integer>> res, int target) {
-        int left = second + 1;
-        int right = nums.length - 1;
-        while (left < right) {
-            int sum = nums[first] + nums[second] + nums[left] + nums[right];
-            if (sum > target) {
-                right--;
-            } else if (sum < target) {
-                left++;
-            } else {
-                res.add(Arrays.asList(nums[first], nums[second], nums[left++], nums[right--]));
-                while (left < right && nums[left - 1] == nums[left]) left++;
-            }
-        }
+            
+        return max;
     }
 }
