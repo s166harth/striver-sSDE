e26621a129251d2aa7f4a51321f440714bbf0193
s166harth <siddharthsagar2019@gmail.com>
2022-06-10 16:11:01 +0530

    unique paths using dp

--- Main.java
+++ Main.java
@@ -1,16 +1,25 @@
+import java.util.Arrays;
+
 class Solution {
     public int uniquePaths(int m, int n) {
-        int g = Math.max(m, n)-1;
-        int num=1;
-        int den=1;
-        for(int i=0;i<g;i++)
-        {
-            num *= m+n-2-i;
-        }
-        for(int i=0;i<g;i++)
-        {
-            den *= g-i;
-        }
-        return (int)(num/den);
-    }
+		int[][] dp = new int[m][n];
+		for (int[] row : dp)
+			Arrays.fill(row, -1);
+		return countPaths(0, 0, m, n, dp);
+	}
+
+	public int countPaths(int i, int j, int m, int n, int[][] dp) {
+		
+		if (i == (m - 1) && j == (n - 1))
+			return 1;
+		if (i >= m || j >= n)
+			return 0;
+
+		
+		if (dp[i][j] != -1)
+			return dp[i][j];
+
+		
+		return dp[i][j] = countPaths(i + 1, j, m, n, dp) + countPaths(i, j + 1, m, n, dp);
+	}
 }
