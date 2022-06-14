629b503a5175e2823ef81f1bbcebac4d355b4531
s166harth <siddharthsagar2019@gmail.com>
2022-06-15 01:27:21 +0530

    deletion from linkedlist when only node is given

--- Main.java
+++ Main.java
@@ -1,35 +1,12 @@
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int exp=0;
-        int sum=0;
-        while(l1!=null&&l2!=null)
-        {
-            sum=sum+ l1*(int)Math.pow(10, exp) + l2*(int)Math.pow(10, exp);
-            exp+=1;
-            l1=l1.next;
-            l2=l2.next;
-
-        }
-        if(l1!=null)
-        {
-            while(l1!=null)
-            {
-                sum=sum+ l1*(int)Math.pow(10, exp);
-            exp+=1;
-            l1=l1.next;
-
-            }
-        }
-        else if(l2!=null)
-        {
-            while(l2!=null)
-            {
-                sum=sum+ l2*(int)Math.pow(10, exp);
-            exp+=1;
-            l2=l2.next;
-
-            }
-        }
-        return sum;
+    public void deleteNode(ListNode node) {
+        ListNode pre = null;
+    while(node.next != null)
+    {
+        pre = node;
+        node.val = node.next.val;
+        node = node.next;
+    }
+    pre.next = null;
     }
 }
