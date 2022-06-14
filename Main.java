2fd08780893ae58f4952ea29fa3756b5a009ddec
s166harth <siddharthsagar2019@gmail.com>
2022-06-14 23:28:47 +0530

    reversing a linked list using 3 pointers

--- Main.java
+++ Main.java
@@ -1,18 +1,43 @@
-import java.util.LinkedList;
 
+
+
+ 
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        LinkedList<Character> list=new LinkedList<>();
-        int max=0;
-        for(int right = 0, left = 0 ; right < s.length() ; right++){
-            while(list.contains(s.charAt(right)) && right >= left){
-                max = Math.max(max, list.size());
-                list.removeFirst();
-                left++;
+    public class ListNode {
+        int val;
+         ListNode next;
+         ListNode() {}
+         ListNode(int val) { this.val = val; }
+         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+    public ListNode reverseList(ListNode head) {
+
+   
+           
+                
+                 if(head == null){
+                    return head;
+                }
+                
+                if(head != null && head.next == null){
+                    return head;
+                }
+                
+                ListNode prev = null;
+                ListNode cur = head;
+                ListNode nex = head.next;
+                while(nex!=null)
+                {
+                    cur.next=prev;
+                    prev=cur;
+                    cur=nex;
+                    nex=nex.next;
+                }
+                cur.next=prev;
+                return cur;
+        
             }
-            list.addLast(s.charAt(right));
-        }
-        max = Math.max(max, list.size());
-        return max;
+        
+            
+
     }
-}
