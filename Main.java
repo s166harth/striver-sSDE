256d3ac811f3141d9b2b3da7c360ece84f8607f2
s166harth <siddharthsagar2019@gmail.com>
2022-06-14 23:05:45 +0530

    longest subsequence with no repeat with two pointers

--- Main.java
+++ Main.java
@@ -1,1 +1,18 @@
-for solving problems revolving around largest syubarray, always remember the hashmap approach which was used in previous problem.
+import java.util.LinkedList;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        LinkedList<Character> list=new LinkedList<>();
+        int max=0;
+        for(int right = 0, left = 0 ; right < s.length() ; right++){
+            while(list.contains(s.charAt(right)) && right >= left){
+                max = Math.max(max, list.size());
+                list.removeFirst();
+                left++;
+            }
+            list.addLast(s.charAt(right));
+        }
+        max = Math.max(max, list.size());
+        return max;
+    }
+}
