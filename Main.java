69978944adfeed6bea7ec6c119d1ad9d7f586217
s166harth <siddharthsagar2019@gmail.com>
2022-06-15 00:15:18 +0530

    merging two linked lists using extra space

--- Main.java
+++ Main.java
@@ -1,13 +1,44 @@
 class Solution {
-    public ListNode middleNode(ListNode head) {
-        ListNode fast=head;
-        ListNode slow = head;
-        while(fast!=null&&fast.next!=null)
-        {
-            slow=slow.next;
-            fast=fast.next.next;
-            
-        }
-        return slow;
-    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) return list2;
+                if(list2 == null) return list1;
+                
+                ListNode node;
+                
+                if(list1.val < list2.val) {
+                    node = new ListNode(list1.val);
+                    list1 = list1.next;
+                }
+                else {
+                    node = new ListNode(list2.val);
+                    list2 = list2.next;
+                }
+                ListNode curr = node;
+                    
+                while (list1 != null && list2 != null) {
+                    if(list1.val < list2.val) {
+                        curr.next = list1;
+                        curr = curr.next;
+                        list1 = list1.next;
+                    }else {
+                        curr.next = list2;
+                        curr = curr.next;
+                        list2 = list2.next;
+                    }
+                }
+                while (list1 != null) {
+                    curr.next = list1;
+                    curr = curr.next;
+                    list1 = list1.next;
+                }
+                while (list2 != null) {
+                    curr.next = list2;
+                    curr = curr.next;
+                    list2 = list2.next;
+                }
+                 curr.next = null;
+                
+                 return node;
+             }
+         }
 }
