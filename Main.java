bbf2bb2e1bdf8e9d2ed2ba0ec4dd18258b2f761b
s166harth <siddharthsagar2019@gmail.com>
2022-06-14 23:52:10 +0530

    middle of linked list using fast and slow

--- Main.java
+++ Main.java
@@ -1,34 +1,13 @@
-
-
-
- 
 class Solution {
-    public ListNode reverseBetween(ListNode head, int left, int right) {
-        int i=1;
-        ListNode dummy = new ListNode(0,head);
-        ListNode prev = dummy;
-        ListNode curr = head;
-        while(i<left){
-           prev = curr;
-           curr = curr.next;
-            i++;
-        }
-        ListNode next = curr.next;
-        while(i<right){
-            ListNode temp = next.next;
-            next.next = curr;
-            curr = next;
-            next = temp;
-            i++;
-        }
-      
-       
-        ListNode first = prev.next;
-        prev.next = curr;
-        first.next = next;
-        
-        return dummy.next;
-    }
+    public ListNode middleNode(ListNode head) {
+        ListNode fast=head;
+        ListNode slow = head;
+        while(fast!=null&&fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
             
-
+        }
+        return slow;
     }
+}
