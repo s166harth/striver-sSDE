0928bb45ecb9a85f438875f0ad809683ba707948
s166harth <siddharthsagar2019@gmail.com>
2022-06-15 20:31:48 +0530

    intersection of lists w styrings

--- Main.java
+++ Main.java
@@ -1,18 +1,31 @@
-public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode nA = headA; 
-        ListNode nB = headB;
-        while(nA!=nB){
-            if(nA==null) // resetting logic
-                nA = headB;
-            else
-                nA = nA.next;
+import java.util.HashMap;
 
-            if(nB==null) // resetting logic
-                nB = headA;
-            else
-                nB = nB.next;
+class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+
+        HashMap<String,Integer> t=new HashMap<>();
+        for(int i=0;i<list1.length;i++)
+        {
+            t.put(list1[i],i);
+
         }
-        return nA;
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i=0;i<list2.length;i++)
+            if(t.containsKey(list2[i]))
+                map.put(list2[i], i + t.get(list2[i]));
+        
+        
+        int min = Collections.min(map.values());
+        ArrayList<String> l = new ArrayList<>();
+        for(Map.Entry<String, Integer> entry:map.entrySet())
+            if(entry.getValue()==min)
+                l.add(entry.getKey());
+        
+        
+        String[] ans = new String[l.size()];
+        for(int i=0;i<ans.length;i++)
+            ans[i] = l.get(i);
+        
+        return ans;
     }
 }
