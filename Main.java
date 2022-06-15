0016246ff2a186032f8e3c58dfc6c7fbafc07c72
s166harth <siddharthsagar2019@gmail.com>
2022-06-15 20:48:10 +0530

    cycle start point with slow fast and temp

--- Main.java
+++ Main.java
@@ -1,17 +1,20 @@
 public class Solution {
-    public boolean hasCycle(ListNode head) {
-        boolean ans = false;
-        ListNode fast=head;
-        ListNode slow=head;
-        while(fast!=null&&fast.next!=null)
-        {
-            fast=fast.next.next;
-            slow=slow.next;
-            if(fast==slow)
-            {
-                ans= true;
-            }
-        }
-        return ans;
+    public ListNode detectCycle(ListNode head) {
+    ListNode slow=head;
+    ListNode fast=head;
+    boolean flag=false;
+    while(fast!=null && fast.next!=null){
+    slow=slow.next;
+    fast=fast.next.next;
+    if(slow==fast) {flag=true;break;}
     }
-}
+    ListNode temp=head;
+    if(flag){
+    while(temp!=slow){
+    temp=temp.next;
+    slow=slow.next;
+    }}
+    else return null;
+    return temp;
+    }
+    }
