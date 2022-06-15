412054e644590ac02160c17e7a61b41f7214f7a7
s166harth <siddharthsagar2019@gmail.com>
2022-06-15 20:38:00 +0530

    detect cycle

--- Main.java
+++ Main.java
@@ -1,31 +1,17 @@
-import java.util.HashMap;
-
-class Solution {
-    public String[] findRestaurant(String[] list1, String[] list2) {
-
-        HashMap<String,Integer> t=new HashMap<>();
-        for(int i=0;i<list1.length;i++)
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        boolean ans = false;
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null&&fast.next!=null)
         {
-            t.put(list1[i],i);
-
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow)
+            {
+                ans= true;
+            }
         }
-        HashMap<String, Integer> map = new HashMap<>();
-        for(int i=0;i<list2.length;i++)
-            if(t.containsKey(list2[i]))
-                map.put(list2[i], i + t.get(list2[i]));
-        
-        
-        int min = Collections.min(map.values());
-        ArrayList<String> l = new ArrayList<>();
-        for(Map.Entry<String, Integer> entry:map.entrySet())
-            if(entry.getValue()==min)
-                l.add(entry.getKey());
-        
-        
-        String[] ans = new String[l.size()];
-        for(int i=0;i<ans.length;i++)
-            ans[i] = l.get(i);
-        
         return ans;
     }
 }
