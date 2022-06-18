a51350130d246b5442e73812a23f0fd2eaeb9098
s166harth <siddharthsagar2019@gmail.com>
2022-06-18 20:21:53 +0530

    inserting new nodes and then segregating the linkedlist from original linkedlist

--- Main.java
+++ Main.java
@@ -1,28 +1,50 @@
+
+
 class Solution {
-    public ListNode rotateRight(ListNode head, int k) {
-    if(k <= 0 || head == null || head.next == null){
-    return head;
-    }
+    public Node copyRandomList(Node head) {
+         if(head==null){
+            return head;
+        }
+        Node iter=head;
+        
+        while(iter!=null)
+        {
+            Node dummy = new Node(iter.val);
+            dummy.next=iter.next;
+            iter.next=dummy;
+            iter=iter.next.next;
+        }
+        iter = head;
     
-     ListNode last = head;
-     int length = 1;
-     while(last.next != null){
-         last = last.next;
-         length++;
-     }
-     
-     last.next = head;
-     int rotation = k % length;
-     int skip = length - rotation;
-     ListNode newLast = head;
-     
-     for(int i = 0; i < skip - 1; i++){
-         newLast = newLast.next;
-     }
-     head = newLast.next;
-     newLast.next = null;
-     
-     return head;
-    
+        while(iter!=null){
+            Node random = iter.random;
+            
+            if(random!=null){
+                iter.next.random = random.next;
+            }
+            
+            iter = iter.next.next;
+        }
+        Node p1 = head;
+        Node p2 = head.next;
+        Node newH = p2;
+        
+        while(true){
+            Node n1 = p2.next;
+            if(n1==null){
+                p1.next = null;
+                break;
+            }
+            
+            Node n2 = n1.next;
+            
+            p1.next = n1;
+            p2.next = n2;
+            
+            p1 = n1;
+            p2=n2;
+        }
+        
+        return newH;
     }
-    }
+}
