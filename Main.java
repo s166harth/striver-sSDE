f4e7de4597bbaee8e37a872857afffe1dda557f5
s166harth <siddharthsagar2019@gmail.com>
2022-06-18 21:41:59 +0530

    maximum consecutive ones

--- Main.java
+++ Main.java
@@ -1,22 +1,16 @@
 class Solution {
-    public int trap(int[] height) {
-        int n = height.length;
-        int[] maxRight = new int[n];
-        int[] maxLeft = new int[n];
-        maxLeft[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            maxLeft[i] = Math.max(height[i], maxLeft[i - 1]);
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max=0;
+        int cur=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==1)cur++;
+            else{
+                max=Math.max(cur, max);
+                cur=0;
+            }
         }
-        maxRight[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            maxRight[i] = Math.max(height[i], maxRight[i + 1]);
-        }
-        int trap = 0;
-        for (int i = 1; i < n - 1; i++) {
-            int min = Math.min(maxRight[i + 1], maxLeft[i - 1]);
-            if (min - height[i] > 0)
-                trap += Math.max(0, min - height[i]);
-        }
-        return trap;
+        max=Math.max(cur, max);
+        return max;
     }
 }
