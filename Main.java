8135f01ed6e7ab04106d649b25f28fe238503620
s166harth <siddharthsagar2019@gmail.com>
2022-06-20 02:03:15 +0530

    job sequencing and comparators

--- Main.java
+++ Main.java
@@ -1,23 +1,42 @@
-
-class Solution
-{
-   //Function to find the minimum number of platforms required at the
-   //railway station such that no train waits.
-   static int findPlatform(int arr[], int dep[], int n)
-   {
-       Arrays.sort(arr);
-       Arrays.sort(dep);
-      
-      int j=0,temp=1;
-      
-      for(int i=1;i<n;i++)
-          if(arr[i]>dep[j])
-             j++;
-          else
-           temp++;
-      
-       return temp;
-   }
-   
+class Sorter implements Comparator<Job>{                                    
+    public int compare(Job o1 ,Job o2){
+        return o2.profit-o1.profit;
+    }
 }
-
+class Solution
+{ 
+   
+    int[] JobScheduling(Job arr[], int n)
+    {
+        int total=0;
+        int count=0;
+        Arrays.sort(arr,new Sorter());
+        boolean[] avail=new boolean[101];
+    
+        for(int i=0;i<arr.length;i++)
+        {
+            if(avail[arr[i].deadline]==false)
+            {
+                total+=arr[i].profit;
+                avail[arr[i].deadline]=true;
+                count++;
+            }
+            else 
+            {
+                for(int j=arr[i].deadline-1;j>=1;j--)
+                {
+                    if(avail[j]==false)
+                    {
+                        count++;
+                        total+=arr[i].profit;
+                        avail[j]=true;
+                        break;
+                    }
+                }
+            }
+        }
+   
+        return new int[]{count,total};
+    }
+    
+}
