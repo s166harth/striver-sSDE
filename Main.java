1f0b10b5d42cc4c8e5391b969d6bd1406529f2e6
s166harth <siddharthsagar2019@gmail.com>
2022-06-20 02:30:20 +0530

    printing sum of all subsets using recursion

--- Main.java
+++ Main.java
@@ -1,36 +1,17 @@
-class Solution
-{
-    //Function to get the maximum total value in the knapsack.
-    double fractionalKnapsack(int W, Item arr[], int n) 
-    {
-        // Your code here
-        
-        Arrays.sort(arr, new Comparator<Item>(){
-            public int compare(Item i1, Item i2){
-                double i2Ratio = (i2.value*1.0)/i2.weight;
-                double i1Ratio = (i1.value*1.0)/i1.weight;
-                if(i2Ratio>i1Ratio){
-                    return 1;
-                }else if(i2Ratio<i1Ratio){
-                    return -1;
-                }
-                    return 0;
-                
-            }
-        });
-        
-        double maxVal = 0;
-        
-        for(Item it:arr){
-            if(W>0){
-                int weightTaken = it.weight <= W ? it.weight : W;
-                maxVal = maxVal + ((it.value*1.0)/it.weight)*weightTaken;
-                W = W-weightTaken;
-            }else{
-                break;
-            }
-            
-        }
-        return maxVal;
+class Solution{
+    ArrayList<Integer> subsetSums(ArrayList<Integer> arr, int N){
+        // code here
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        Collections.sort(arr);
+        req(0,list,arr,N,0);
+        return list;
     }
-}
+    void req(int index,ArrayList<Integer> list,ArrayList<Integer> arr,int N,int sum){
+        list.add(sum);
+        if(index==N){ return ; }
+        for(int i=index; i<N; i++){
+            req(i+1,list,arr,N,sum+arr.get(i));
+        }
+        
+    }
+ }
