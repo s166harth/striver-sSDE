545d1c5ab545141f23b136ce90615ea48bb8b7e6
s166harth <siddharthsagar2019@gmail.com>
2022-06-20 06:48:16 +0530

    kth permutation using the logic of eliminating using number of permutation

--- Main.java
+++ Main.java
@@ -1,38 +1,27 @@
 class Solution {
-    public boolean isPali(String str){
-        int i = 0,j = str.length()-1;
-        while(i<j){
-            if(str.charAt(i)!=str.charAt(j)) return false;
-            i++;
-            j--;
+    public String getPermutation(int n, int k) {
+        int fact = 1;
+        List<Integer> ds = new ArrayList<>();
+        for (int i = 1; i < n; i++) {
+            fact = fact * i;
+            ds.add(i);
         }
-        return true;
-    }
-    public void solve(String s , int i,int j,List<List<String>> ans,List<String> tmp){
-        if(i>j){
-            if(tmp.size()!=0){
-            ans.add(new ArrayList<>(tmp));
+        ds.add(n);
+        
+        StringBuilder ans = new StringBuilder();
+        k -= 1;
+        
+        while (true) {
+            ans.append(ds.get(k / fact));
+            ds.remove(k / fact);
+            
+            if (ds.size() == 0) {
+                break;
             }
-            return;
+            
+            k %= fact;
+            fact /= ds.size();
         }
-        tmp.add(s.substring(i,i+1));
-        solve(s,i+1,j,ans,tmp);
-        tmp.remove(tmp.size()-1);
-        for(int k = i+1;k<=j;k++){
-            String P = s.substring(i,k+1);
-            if(isPali(P)){
-                tmp.add(P);
-                solve(s,k+1,j,ans,tmp);
-                tmp.remove(tmp.size()-1);
-            }
-        }
-    }
-    public List<List<String>> partition(String s) {
-        int n = s.length();
-        List<List<String>> ans = new ArrayList<>();
-        List<String> tmp = new ArrayList<>();
-        solve(s,0,n-1,ans,tmp);
-        return ans;
-       
+        return ans.toString();
     }
 }
