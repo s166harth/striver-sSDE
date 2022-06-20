1ea99ff942f7f3bf6207f2c6e708a4b167c95c37
s166harth <siddharthsagar2019@gmail.com>
2022-06-20 06:05:32 +0530

    combinations 2 , same as combinations 1

--- Main.java
+++ Main.java
@@ -1,24 +1,25 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] arr, int target) {
+    public List<List<Integer>> combinationSum2(int[] arr, int target) {
+        Arrays.sort(arr);
         List<List<Integer>> ans = new ArrayList<>();
-        
         findCombinations(0, arr, target, ans, new ArrayList<>());
+        
         return ans;
     }
     
     public void findCombinations(int ind, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds){
-        if(ind == arr.length){
-            if(target == 0){
-                ans.add(new ArrayList<>(ds));
-            }
+        if(target == 0){
+            ans.add(new ArrayList<>(ds));
             return;
         }
         
-        if(arr[ind] <= target){
-            ds.add(arr[ind]);
-            findCombinations(ind, arr, target - arr[ind], ans, ds);
+        for(int i=ind; i<arr.length; i++){
+            if(i > ind && arr[i] == arr[i-1]) continue;
+            if(arr[i] > target) break;
+            
+            ds.add(arr[i]);
+            findCombinations(i + 1, arr, target - arr[i], ans, ds);
             ds.remove(ds.size() - 1);
         }
-        findCombinations(ind + 1, arr, target, ans, ds);
     }
 }
