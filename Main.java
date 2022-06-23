56fd4e69d23b2083f5625a5db0388525551c9f65
s166harth <siddharthsagar2019@gmail.com>
2022-06-23 22:30:47 +0530

    elements of a priority queue are a max heap by default , to make it a min heap just use negative of each number and all heaps can be shown as a priority queue

--- Main.java
+++ Main.java
@@ -1,29 +1,19 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int result = binarySearch(nums, 0, nums.length - 1, target);
-        return result;
-
-    }
-
-    public int binarySearch(int[] nums, int low, int high, int target) {
-        if (high >= low) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            }
-            if (nums[mid] > target) {
-                int res1 = binarySearch(nums, low, mid - 1, target);
-                if (res1 == -1) {
-                    res1 = binarySearch(nums, mid + 1, high, target);
+    public int findKthLargest(int[] nums, int k) {
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i : nums) {
+            if(pq.size() < k) {
+                pq.add(i);
+            } else {
+                if(i > pq.peek()) {
+                    pq.remove();
+                    pq.add(i);
                 }
-                return res1;
             }
-            int res2 = binarySearch(nums, mid + 1, high, target);
-            if (res2 == -1) {
-                res2 = binarySearch(nums, low, mid - 1, target);
-            }
-            return res2;
         }
-        return -1;
+        
+        return pq.peek();
     }
 }
