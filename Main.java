8dc4b90e462d354b5aff44a971aa9c3edef3839b
s166harth <siddharthsagar2019@gmail.com>
2022-06-23 22:49:20 +0530

    priority queue with a comparator

--- Main.java
+++ Main.java
@@ -1,19 +1,28 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
-        for(int i : nums) {
-            if(pq.size() < k) {
-                pq.add(i);
-            } else {
-                if(i > pq.peek()) {
-                    pq.remove();
-                    pq.add(i);
-                }
+
+    public int[] topKFrequent(int[] nums, int k) {
+        if(nums.length == 0){
+            return new int[k];
+        }
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->(map.get(a)-map.get(b)));
+        for(int n : map.keySet()){
+            pq.add(n);
+            if(pq.size()>k){
+                pq.poll();
             }
         }
+        int i = 0;
+        int[] result = new int[k];
+        while(pq.size()!=0){
+            int rem = pq.poll();
+            result[i++] = rem;
+        }
         
-        return pq.peek();
+        return result;
     }
-}
+    
+    }
