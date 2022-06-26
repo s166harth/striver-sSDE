29ce2fd1d002f4d06f612560daff51d542af67a8
s166harth <siddharthsagar2019@gmail.com>
2022-06-27 04:05:11 +0530

    rabin karp

--- Main.java
+++ Main.java
@@ -1,14 +1,45 @@
-class StockSpanner {
-    Stack<Pair<Integer,Integer>>st;
-    public StockSpanner() {
-        st=new Stack<>();
+class Solution {
+    public int repeatedStringMatch(String a, String b) {
+        StringBuilder s = new StringBuilder(a);
+        int count=1;
+        while(s.length()<b.length()){
+            count++;
+            s.append(a);
+        }
+        
+        if(searchByRabin(s.toString(),b)) return count;
+        s.append(a);
+        if(searchByRabin(s.toString(),b)) return count+1;
+        return -1;
     }
     
-    public int next(int price) {
-        int span=1;
-        while(!st.isEmpty()&&st.peek().getKey()<=price)
-            span+=st.pop().getValue();
-        st.push(new Pair(price,span));
-        return span;      
+    private boolean searchByRabin(String s, String pat){
+        int p=0,t=0;
+        int q=101;
+        int h=1;
+        int d=256;
+        int m=pat.length(),n=s.length();
+        for(int i=0;i<m-1;i++) h = (h*d)%q;
+        for(int i=0;i<m;i++){
+            p = (d*p+pat.charAt(i))%q;
+            t = (d*t+s.charAt(i))%q;
+        }
+        for(int i=0;i<=n-m;i++){
+            if(p==t){
+                boolean match = true;
+                for(int j=0;j<m;j++){
+                    if(pat.charAt(j)!=s.charAt(i+j)) {
+                        match = false;
+                        break;
+                    }
+                    if(match) return true;
+                }
+            }
+            if(i<n-m){
+                t = (d*(t-h*s.charAt(i))+s.charAt(i+m))%q;
+                if(t<0) t += q;
+            }
+        }
+        return false;
     }
 }
