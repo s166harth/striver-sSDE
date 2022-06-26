24877eb394921bbf2b1c29239a0fad348415d75e
s166harth <siddharthsagar2019@gmail.com>
2022-06-27 03:00:13 +0530

    next smallestelement using stacks

--- Main.java
+++ Main.java
@@ -1,28 +1,41 @@
-class Solution {
+import java.util.*;
 
-    public int[] topKFrequent(int[] nums, int k) {
-        if(nums.length == 0){
-            return new int[k];
-        }
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int num : nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->(map.get(a)-map.get(b)));
-        for(int n : map.keySet()){
-            pq.add(n);
-            if(pq.size()>k){
-                pq.poll();
-            }
-        }
-        int i = 0;
-        int[] result = new int[k];
-        while(pq.size()!=0){
-            int rem = pq.poll();
-            result[i++] = rem;
-        }
-        
-        return result;
-    }
-    
-    }
+import java.io.*;
+
+ 
+
+public class Solution{
+
+   static ArrayList<Integer> nextSmallerElement(ArrayList<Integer> arr, int n){
+
+       // Write your code here.
+
+       Stack<Integer> s = new Stack<>();
+
+       s.push(-1);
+
+       ArrayList<Integer> ans = new ArrayList<>();
+
+       for(int i=n-1;i>=0;i--){
+
+           int cur = arr.get(i);
+
+           while(s.peek()>=cur){
+
+               s.pop();
+
+           }
+
+           ans.add(s.peek());
+
+           s.push(cur);
+
+       }
+
+       Collections.reverse(ans);
+
+       return ans;
+
+   }
+
+}
