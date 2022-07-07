9ab4f43783c759e86ecf4edc626ac76cb7d65dac
s166harth <siddharthsagar2019@gmail.com>
2022-07-07 06:58:04 +0530

    needle and haystack by not using index of

--- Main.java
+++ Main.java
@@ -1,46 +1,15 @@
 class Solution {
-    public int repeatedStringMatch(String a, String b) {
-        StringBuilder s = new StringBuilder(a);
-        int count=1;
-        while(s.length()<b.length()){
-            count++;
-            s.append(a);
-        }
-        
-        if(searchByRabin(s.toString(),b)) return count;
-        s.append(a);
-        if(searchByRabin(s.toString(),b)) return count+1;
-        return -1;
-    }
-    
-    private boolean searchByRabin(String s, String pat){
-        int p=0,t=0;
-        int q=101;
-        int h=1;
-        int d=256;
-        int m=pat.length(),n=s.length();
-        for(int i=0;i<m-1;i++) h = (h*d)%q;
-        for(int i=0;i<m;i++){
-            p = (d*p+pat.charAt(i))%q;
-            t = (d*t+s.charAt(i))%q;
-        }
-        for(int i=0;i<=n-m;i++){
-            if(p==t){
-                boolean match = true;
-                for(int j=0;j<m;j++){
-                    if(pat.charAt(j)!=s.charAt(i+j)) {
-                        match = false;
-                        break;
-                    }
-                    if(match) return true;
+    public int strStr(String haystack, String needle) {
+        int nl=needle.length();
+        int hl=haystack.length();
+        if(nl==0) return 0;
+        for(int i=0;i<hl;i++){
+            if(i+nl<=hl){
+                if(needle.equals(haystack.substring(i,i+nl))){
+                    return i;
                 }
             }
-            if(i<n-m){
-                t = (d*(t-h*s.charAt(i))+s.charAt(i+m))%q;
-                if(t<0) t += q;
-            }
         }
-        return false;
+        return -1;
     }
 }
-hello
