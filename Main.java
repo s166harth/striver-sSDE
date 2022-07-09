a03317f584e57083c6b1de11128cee28a6725b99
s166harth <siddharthsagar2019@gmail.com>
2022-07-09 21:54:49 +0530

    searching bst

--- Main.java
+++ Main.java
@@ -1,19 +1,10 @@
 class Solution {
-    // Function to convert a binary tree into its mirror tree.
-    void mirror(Node node) {
-        // Your code here
-        if(node==null)
-        {
-            return;
-        }
-         mirror(node.right);
-        mirror(node.left);
-     
-            Node temp=node.left;
-            
-            node.left=node.right;
-            node.right=temp;
-       
-        return;
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root==null || root.val==val)
+            return root;
+        else if(root.val>val)
+            return searchBST(root.left,val);
+        else
+            return searchBST(root.right,val);
     }
 }
