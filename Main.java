16ea29be2384527ab272cb93e388f3a43a81e2cb
s166harth <siddharthsagar2019@gmail.com>
2022-07-10 08:38:57 +0530

    lca bst

--- Main.java
+++ Main.java
@@ -1,28 +1,7 @@
-class Solution {
-    // public boolean isValidBST(TreeNode root) {
-    //     if(root.right==null && root.left==null) return true;
-    //     if(root.right==null&&root.left!=null)
-    //     {
-    //         if(root.left.val<root.val)return isValidBST(root.left);
-    //         else return false;
-    //     }
-    //     if(root.left==null&&root.right!=null)
-    //     {
-    //         if(root.right.val>root.val)return isValidBST(root.right);
-    //         else return false;
-    //     }
-    //     return root.left.val<root.val && root.right.val>root.val && isValidBST(root.right) && isValidBST(root.left);
-    // }
-    // wrong because you are checking the indivisual node only and not entire tree so works for only one level
-    public boolean isValidBST(TreeNode root) {
-        return isValidBSTHelper(root,Long.MIN_VALUE,Long.MAX_VALUE);
+public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if((root.val == p.val) || (root.val == q.val)){return root;}
+    else if(root.val>p.val && root.val<q.val){return root;}
+    else if(root.val<p.val && root.val>q.val){return root;}
+    else if(root.val>p.val && root.val>q.val){return lowestCommonAncestor(root.left,p,q);}
+    else{return lowestCommonAncestor(root.right,p,q);}
     }
-
-    private boolean isValidBSTHelper(TreeNode root, long minValue, long maxValue) {
-        if(root == null) return true;
-        
-        if(root.val >= maxValue || root.val <= minValue) return false;
-        return (isValidBSTHelper(root.left,minValue, root.val) && 
-                isValidBSTHelper(root.right, root.val, maxValue));
-    }
-}
