b940c215a55bc87f1cf652b2f207416a6c97a371
s166harth <siddharthsagar2019@gmail.com>
2022-07-10 09:50:17 +0530

    2 sum with sorted array

--- Main.java
+++ Main.java
@@ -1,10 +1,26 @@
 class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q) return root;
-        var left = lowestCommonAncestor(root.left, p, q);
-        var right = lowestCommonAncestor(root.right, p, q);
-        if (left == null) return right;
-        if (right == null) return left;
-        return root;
+    public int[] twoSum(int[] numbers, int target) {
+        int[] ans = new int[2];
+        int s=0;
+        int end = numbers.length-1;
+        while(s<e)
+        {
+            if(numbers[s]+numbers[end]<target)
+            {
+                s++;
+            }
+            if(numbers[s]+numbers[end]>target)
+            {
+                end--;
+            }
+            if(numbers[s]+numbers[end]==target)
+            {
+                ans[0]=numbers[s];
+        ans[1]=numbers[end];
+        return ans;
+            }
+        }
+        return null;
+        
     }
 }
