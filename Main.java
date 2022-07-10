46a0143d5f773b3318a22c89685f033dcd843513
s166harth <siddharthsagar2019@gmail.com>
2022-07-10 08:41:16 +0530

    lca in bt

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
-public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-    if((root.val == p.val) || (root.val == q.val)){return root;}
-    else if(root.val>p.val && root.val<q.val){return root;}
-    else if(root.val<p.val && root.val>q.val){return root;}
-    else if(root.val>p.val && root.val>q.val){return lowestCommonAncestor(root.left,p,q);}
-    else{return lowestCommonAncestor(root.right,p,q);}
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        var left = lowestCommonAncestor(root.left, p, q);
+        var right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) return right;
+        if (right == null) return left;
+        return root;
     }
+}
