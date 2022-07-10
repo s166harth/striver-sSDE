89334802ca0d2f131cd1827f802e9b834766ad5f
s166harth <siddharthsagar2019@gmail.com>
2022-07-10 07:28:02 +0530

    bst from 2d matrix using hashmaps

--- Main.java
+++ Main.java
@@ -1,33 +1,83 @@
-class Solution {
+// class Solution {
+//     public TreeNode search(TreeNode parent,int val)
+//     {
+//         if(val==parent.val) return parent;
+//         else if(val<parent.val) return search(parent.right);
+//         else if(val>parent.val) return search(parent.left);
+//         return null;
 
-    public TreeNode listToTree(ListNode head)
-    {
-        if(head == null) return null;
-        if(head.next == null) return new TreeNode(head.val);
-        ListNode start = head;
-        ListNode slow = head;
-        ListNode mid = head;
-        ListNode fast = head;
-        while(fast != null && fast.next != null)
-        {
-            mid = slow;
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        mid.next = null;
-        ListNode secondHead = slow.next;
-        TreeNode node = new TreeNode(slow.val);
-        node.left = listToTree(start);
-        node.right = listToTree(secondHead);``
-    
-        return node;
-    
-        }
-        public TreeNode sortedListToBST(ListNode head)
-        {
-            if(head == null) return null;
-            if(head.next == null) return new TreeNode(head.val);
-            return listToTree(head);
+//     }
+//     public TreeNode createBinaryTree(int[][] d) {
+//         TreeNode cur = new TreeNode(null);
+//         TreeNode root=new TreeNode(null);
+        
+//         for(int i=0;i<d.length;i++)
+//         {
+//             if(search(root,d[i][0]))
+//             {
+//                cur = search(root,d[i][0]);
+//             }
+//             else cur.val = d[i][0];
+//             if(search(root,d[i][1])&&d[i][2]==1)
+//             {
+//                 cur.left = search(root,d[i][1]);
+//             }
+//             else if(search(root,d[i][1])&&d[i][2]==0)
+//             {
+//                 cur.right = search(root,d[i][1]);
+//             }
+//             else if(!search(root,d[i][1])&&d[i][2]==1)
+//             {
+//                 cur.left = new TreeNode(d[i][1]);
+
+//             }
+//             else if(!search(root,d[i][1])&&d[i][2]==0)
+//             {
+//                 cur.right = new TreeNode(d[i][1]);
+                
+//             }
+//             cur = root;
+
             
+
+//         }
+//     }
+// }
+class Solution {
+    HashMap<Integer, TreeNode> map;
+    HashMap<Integer, TreeNode> childSet;
+    public TreeNode createBinaryTree(int[][] description) {
+        childSet = new HashMap<>();
+        map = new HashMap<>();
+        for(int i = 0; i < description.length; i++){
+            int node = description[i][0];
+            int child = description[i][1];
+            boolean left = description[i][2] == 1;
+            TreeNode root, childNode;
+            if(map.containsKey(node))
+                root = map.get(node);
+            else{
+                root = new TreeNode(node);
+                map.put(node, root);
+            }
+            
+            if(map.containsKey(child))
+                childNode = map.get(child);
+            else{
+                childNode = new TreeNode(child);
+                map.put(child, childNode);
+            }
+            if(left)
+                root.left = childNode;
+            else
+                root.right = childNode;
+            
+            childSet.put(child, childNode); 
         }
+        for(int i = 0; i < description.length; i++){
+            if(!childSet.containsKey(description[i][0]))
+                return map.get(description[i][0]);
+        }
+        return null;
     }
+}
