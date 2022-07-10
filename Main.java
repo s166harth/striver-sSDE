a5a01c2e9fd1c5821eb3e4c258c422830fc1d7c7
s166harth <siddharthsagar2019@gmail.com>
2022-07-10 07:59:08 +0530

    is valid bst using min and max

--- Main.java
+++ Main.java
@@ -1,83 +1,28 @@
-// class Solution {
-//     public TreeNode search(TreeNode parent,int val)
-//     {
-//         if(val==parent.val) return parent;
-//         else if(val<parent.val) return search(parent.right);
-//         else if(val>parent.val) return search(parent.left);
-//         return null;
-
-//     }
-//     public TreeNode createBinaryTree(int[][] d) {
-//         TreeNode cur = new TreeNode(null);
-//         TreeNode root=new TreeNode(null);
-        
-//         for(int i=0;i<d.length;i++)
-//         {
-//             if(search(root,d[i][0]))
-//             {
-//                cur = search(root,d[i][0]);
-//             }
-//             else cur.val = d[i][0];
-//             if(search(root,d[i][1])&&d[i][2]==1)
-//             {
-//                 cur.left = search(root,d[i][1]);
-//             }
-//             else if(search(root,d[i][1])&&d[i][2]==0)
-//             {
-//                 cur.right = search(root,d[i][1]);
-//             }
-//             else if(!search(root,d[i][1])&&d[i][2]==1)
-//             {
-//                 cur.left = new TreeNode(d[i][1]);
-
-//             }
-//             else if(!search(root,d[i][1])&&d[i][2]==0)
-//             {
-//                 cur.right = new TreeNode(d[i][1]);
-                
-//             }
-//             cur = root;
-
-            
-
-//         }
-//     }
-// }
 class Solution {
-    HashMap<Integer, TreeNode> map;
-    HashMap<Integer, TreeNode> childSet;
-    public TreeNode createBinaryTree(int[][] description) {
-        childSet = new HashMap<>();
-        map = new HashMap<>();
-        for(int i = 0; i < description.length; i++){
-            int node = description[i][0];
-            int child = description[i][1];
-            boolean left = description[i][2] == 1;
-            TreeNode root, childNode;
-            if(map.containsKey(node))
-                root = map.get(node);
-            else{
-                root = new TreeNode(node);
-                map.put(node, root);
-            }
-            
-            if(map.containsKey(child))
-                childNode = map.get(child);
-            else{
-                childNode = new TreeNode(child);
-                map.put(child, childNode);
-            }
-            if(left)
-                root.left = childNode;
-            else
-                root.right = childNode;
-            
-            childSet.put(child, childNode); 
-        }
-        for(int i = 0; i < description.length; i++){
-            if(!childSet.containsKey(description[i][0]))
-                return map.get(description[i][0]);
-        }
-        return null;
+    // public boolean isValidBST(TreeNode root) {
+    //     if(root.right==null && root.left==null) return true;
+    //     if(root.right==null&&root.left!=null)
+    //     {
+    //         if(root.left.val<root.val)return isValidBST(root.left);
+    //         else return false;
+    //     }
+    //     if(root.left==null&&root.right!=null)
+    //     {
+    //         if(root.right.val>root.val)return isValidBST(root.right);
+    //         else return false;
+    //     }
+    //     return root.left.val<root.val && root.right.val>root.val && isValidBST(root.right) && isValidBST(root.left);
+    // }
+    // wrong because you are checking the indivisual node only and not entire tree so works for only one level
+    public boolean isValidBST(TreeNode root) {
+        return isValidBSTHelper(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+
+    private boolean isValidBSTHelper(TreeNode root, long minValue, long maxValue) {
+        if(root == null) return true;
+        
+        if(root.val >= maxValue || root.val <= minValue) return false;
+        return (isValidBSTHelper(root.left,minValue, root.val) && 
+                isValidBSTHelper(root.right, root.val, maxValue));
     }
 }
