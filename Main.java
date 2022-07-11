9b919adff35e6cee0f625ee11306f9aac2cc31d8
s166harth <siddharthsagar2019@gmail.com>
2022-07-11 15:49:04 +0530

    clone graph using bfs

--- Main.java
+++ Main.java
@@ -1,28 +1,63 @@
-import java.util.PriorityQueue;
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Queue;
 
-class KthLargest {
-    int k;
-    PriorityQueue<Integer> pq;
-    public KthLargest(int k, int[] nums) {
-        this.k = k;
-        pq = new PriorityQueue<>(k);
-        for(int i=0;i<nums.length;i++)
-        {
-            add(nums[i]);
-        }
 
+
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
     }
-    
-    public int add(int val) {
-        if(pq.size()<k)
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if(node==null) return null;
+        Queue<Node> q = new ArrayDeque<>();
+        HashMap<Node,Node> hm = new HashMap<>();
+        Node start = new Node(node.val);
+        q.add(node);
+        hm.put(node, start);
+        while(!q.isEmpty())
         {
-            pq.add(val);
+            Node cur = q.poll();
+            if(!hm.containsKey(cur))
+            {
+                Node newnode = new Node(cur.val);
+                hm.put(cur, newnode);
+                q.add(newnode);
+            }
+            List<Node> nbr = cur.neighbors;
+            Node n1 = hm.get(cur);
+            for(Node nbrs : nbr)
+            {
+                if(!hm.containsKey(nbrs))
+                {
+                    Node n2 = new Node(nbrs.val);
+                    hm.put(nbrs, n2);
+                    q.add(nbrs);
+                }
+                Node n2 = hm.get(nbrs);
+                n1.neighbors.add(n2);
+            }
+
+            
         }
-        else
-        {
-            pq.add(val);
-            pq.poll();
-        }
-        return pq.peek();
+        return start;
+
     }
 }
