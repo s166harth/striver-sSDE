543d278aa50aca8a3fc98a032de8477746ccf79c
s166harth <siddharthsagar2019@gmail.com>
2022-07-11 16:43:22 +0530

    dfs of graph

--- Main.java
+++ Main.java
@@ -1,35 +1,27 @@
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 
 class Solution {
-    // Function to return Breadth First Traversal of given graph.
-    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
-        // Code here
-        
-        ArrayList<Integer> result = new ArrayList<>();
-        
-        Queue<Integer> q = new LinkedList<>();
-        q.add(0);
-        
-        boolean[] visited = new boolean[V];
-        visited[0] = true;
-        
-        while(!q.isEmpty()) {
-            int v = q.poll();
-            result.add(v);
-            
-            ArrayList<Integer> adjList = adj.get(v);
-            for(int i : adjList) {
-                if(!visited[i]) {
-                    visited[i] = true;
-                    q.add(i);    
-                }
+    public static void dfs(int node, boolean vis[], ArrayList<ArrayList<Integer>> adj, ArrayList<Integer> storeDfs) {
+        storeDfs.add(node);
+        //marking current node as visited
+        vis[node] = true;
+
+        //getting neighbour nodes
+        for(Integer it: adj.get(node)) {
+            if(vis[it] == false) {
+                dfs(it, vis, adj, storeDfs);
             }
         }
-        
-        return result;
+    }
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        boolean[] vis = new boolean[V+1];
+        for(int i=1;i<V+1;i++)
+        {
+            if(!vis[i]) dfs(i,vis,adj,ans);
+        }
+        return ans;
+
     }
 }
