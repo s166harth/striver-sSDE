5de0fa879d52f2e63f136478db54dfa5b1092b8f
s166harth <siddharthsagar2019@gmail.com>
2022-07-11 15:56:43 +0530

    bfs of graph

--- Main.java
+++ Main.java
@@ -1,63 +1,35 @@
 import java.util.ArrayDeque;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-
-
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-
-
 class Solution {
-    public Node cloneGraph(Node node) {
-        if(node==null) return null;
-        Queue<Node> q = new ArrayDeque<>();
-        HashMap<Node,Node> hm = new HashMap<>();
-        Node start = new Node(node.val);
-        q.add(node);
-        hm.put(node, start);
-        while(!q.isEmpty())
-        {
-            Node cur = q.poll();
-            if(!hm.containsKey(cur))
-            {
-                Node newnode = new Node(cur.val);
-                hm.put(cur, newnode);
-                q.add(newnode);
-            }
-            List<Node> nbr = cur.neighbors;
-            Node n1 = hm.get(cur);
-            for(Node nbrs : nbr)
-            {
-                if(!hm.containsKey(nbrs))
-                {
-                    Node n2 = new Node(nbrs.val);
-                    hm.put(nbrs, n2);
-                    q.add(nbrs);
-                }
-                Node n2 = hm.get(nbrs);
-                n1.neighbors.add(n2);
-            }
-
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        
+        boolean[] visited = new boolean[V];
+        visited[0] = true;
+        
+        while(!q.isEmpty()) {
+            int v = q.poll();
+            result.add(v);
             
+            ArrayList<Integer> adjList = adj.get(v);
+            for(int i : adjList) {
+                if(!visited[i]) {
+                    visited[i] = true;
+                    q.add(i);    
+                }
+            }
         }
-        return start;
-
+        
+        return result;
     }
 }
