8cce43801d56d73b10c1f80b3938d4883055b32d
s166harth <siddharthsagar2019@gmail.com>
2022-07-14 23:55:18 +0530

    finding first and last index in sorted array

--- Main.java
+++ Main.java
@@ -1,18 +1,50 @@
 class Solution {
-    public int maxProduct(int[] nums) {
-        int max=nums[0],min=nums[0],res=nums[0],n=nums.length;
-        
-        for(int i=1;i<n;i++){
-            if(nums[i]<0){
-                int temp=max;
-                max=min;
-                min=temp;
-            }
-            max=Math.max(nums[i],max*nums[i]);
-            min=Math.min(nums[i],min*nums[i]);
-            res=Math.max(res,max);
-        }
-        return res;
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = new int[2];
+        ans[0] = firstPosition(nums, target);
+        ans[1] = lastPosition(nums, target);
+        return ans;
     }
     
+    // function for first position (leftmost position)
+    private static int firstPosition(int[] nums, int target){
+        int si = 0;
+        int ei = nums.length-1;
+        int pa = -1; // possible ans
+        while(si <= ei){
+            int mid = (si + ei)/2;
+            if(nums[mid] == target){
+                pa = mid;
+                ei = mid -1; // continue searching in its left
+            }
+            else if(nums[mid] < target){
+                si = mid+1;
+            }
+            else{
+                ei = mid -1;
+            }
+        }
+        return pa;
+    }
+    
+    // function for finding last position (righmost position)
+    private static int lastPosition(int[] nums, int target){
+        int si = 0;
+        int ei = nums.length-1;
+        int pa = -1;
+        while(si <= ei){
+            int mid = (si+ ei)/2;
+            if(nums[mid] == target){
+                pa = mid;
+                si = mid+1; // continue searching in its right
+            }
+            else if(nums[mid] < target){
+                si = mid +1;
+            }
+            else{
+                ei = mid -1;
+            }
+        }
+        return pa;
+    }
 }
